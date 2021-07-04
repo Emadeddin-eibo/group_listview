1691b2359bf857430eaff0442cb52243d4d3d34f
Emadeddin <emadeddin.eibo@gmail.com>
2021-07-04 15:10:09 +0300

    Search with Grouping

--- lib/group_view.dart
+++ lib/group_view.dart
@@ -1,97 +1,99 @@
 // @dart=2.9
 import 'package:flutter/material.dart';
 
-class GroupView extends StatefulWidget {
-  final List<String> items;
+class GroupView<ItemType, HeaderType> extends StatefulWidget {
+  final List<ItemType> items;
+  final String search;
+  final HeaderType Function(ItemType) groupBy;
+  final Widget Function(BuildContext context, ItemType) itemBuilder;
+  final Widget Function(BuildContext context, HeaderType) headerBuilder;
 
   GroupView({
     Key key,
     this.items,
+    this.search,
+    this.groupBy,
+    this.itemBuilder,
+    this.headerBuilder,
   }) : super(key: key) {
-    items.sort();
+    if (items.first is Comparable)
+      items.sort();
+    else
+      items.sort((item1, item2) =>
+          ('${groupBy(item1)}').compareTo('${groupBy(item2)}'));
   }
 
   @override
-  _GroupViewState createState() => _GroupViewState();
+  _GroupViewState<ItemType, HeaderType> createState() => _GroupViewState();
 }
 
-class _GroupViewState extends State<GroupView> {
-  Map<String, List<String>> groupedItems = {};
+class _GroupViewState<ItemType, HeaderType> extends State<GroupView> {
+  Map<HeaderType, List<ItemType>> groupedItems = {};
+  List<ItemType> items = [];
 
   @override
   void initState() {
+    items = widget.items;
     groupItems();
     super.initState();
   }
 
   groupItems() {
-    for (int i = 0; i < widget.items.length; i++) {
-      var object = widget.items[i];
+    setState(() {
+      for (int i = 0; i < items.length; i++) {
+        ItemType object = items[i];
+        HeaderType key = widget.groupBy(items[i]);
 
-      if (!groupedItems.containsKey(object[0])) {
-        groupedItems[object[0]] = [];
+        if (!groupedItems.containsKey(key)) {
+          groupedItems[key] = [];
+        }
+
+        groupedItems.update(
+          key,
+          (value) => value..add(object),
+        );
       }
-
-      groupedItems.update(
-        object[0],
-        (value) => value..add(object),
-      );
-    }
+    });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final List keys = groupedItems.keys.toList();
-    return ListView.builder(
-      itemCount: groupedItems.keys.length,
-      itemBuilder: (context, index) {
-        final currentKey = keys[index];
-        return _ValuesRow(
-          text: currentKey,
-          items: groupedItems[currentKey],
-        );
+  filterItems() {
+    setState(
+      () {
+        groupedItems = {};
+        items = widget.items;
+        if (widget.search.isEmpty) return;
+        items = widget.items
+            .where(
+              (element) => element.toLowerCase().contains(
+                    widget.search.toLowerCase(),
+                  ),
+            )
+            .toList();
       },
     );
   }
-}
-
-class _KeyRow extends StatelessWidget {
-  final String text;
-
-  const _KeyRow({Key key, this.text}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      color: Colors.grey[400],
-      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-      child: Text(text),
-    );
-  }
-}
+    if (widget.search != null) {
+      filterItems();
+      groupItems();
+    }
 
-class _ValuesRow extends StatelessWidget {
-  final List<String> items;
-  final String text;
-
-  const _ValuesRow({Key key, this.items, this.text}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        _KeyRow(text: text),
-        ...items
-            .map(
-              (e) => Container(
-                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                child: Text(e),
-              ),
-            )
-            .toList(),
-      ],
+    return ListView.builder(
+      itemCount: groupedItems.keys.length,
+      itemBuilder: (context, index) {
+        final currentKey = groupedItems.keys.toList()[index];
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            widget.headerBuilder(context, currentKey),
+            ...groupedItems[currentKey]
+                .map((e) => widget.itemBuilder(context, e))
+                .toList(),
+          ],
+        );
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,12 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   final items = <String>[
     'Poppie Hart',
     'Aarron Goodman',
@@ -46,7 +51,7 @@ class Home extends StatelessWidget {
     'Jac Schneider',
   ];
 
-
+  String query = '';
 
   @override
   Widget build(BuildContext context) {
@@ -54,10 +59,50 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text('Goupe List Example'),
       ),
-      body: GroupView(
-        items: items,
+      body: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(hintText: 'Type to search...'),
+            onChanged: (text) => setState(() {
+              query = text;
+            }),
+          ),
+          Expanded(
+            child: GroupView(
+              items: items,
+              search: query,
+              groupBy: (item) => item[0],
+              headerBuilder: (context, headerObject) {
+                return Container(
+                  width: MediaQuery.of(context).size.width,
+                  color: Colors.grey[400],
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  child: Text(headerObject),
+                );
+              },
+              itemBuilder: (context, item) {
+                return Container(
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  child: Text(item.toString()),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
 }
 
+class Item {
+  final int id;
+  final String name;
+
+  Item(
+    this.id,
+    this.name,
+  );
+
+  @override
+  String toString() => '{id: $id, name: $name}';
+}
